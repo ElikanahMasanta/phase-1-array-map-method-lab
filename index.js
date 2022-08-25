d302d3ee974400a4ff811ef53454b3b9cf0e1796
ElikanahMasanta <emasanta8@gmail.com>
2022-08-25 11:49:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (tutorials) => {
+  return tutorials.split("").map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  });
+
 }
